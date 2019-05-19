e758548c9980e9561122853a3964513a57808f50
Bondaruk Vadim <v.bondaruk@softteco.com>
2019-05-19 20:48:12 +0300

    Add project files.

---
+++ ProxyDemo/Middleware/ProxyMiddleware.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ProxyDemo.Middleware
+{
+    public class ProxyMiddleware : IMiddleware
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var targetUri = BuildTargetUri(context.Request);
+
+            if (targetUri != null)
+            {
+                var targetRequestMessage = CreateTargetMessage(context, targetUri);
+
+                using (var responseMessage = await _httpClient.SendAsync(targetRequestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
+                {
+                    context.Response.StatusCode = (int)responseMessage.StatusCode;
+                    CopyFromTargetResponseHeaders(context, responseMessage);
+                    await responseMessage.Content.CopyToAsync(context.Response.Body);
+                }
+                return;
+            }
+            await next(context);
+        }
+
+        private HttpRequestMessage CreateTargetMessage(HttpContext context, Uri targetUri)
+        {
+            var requestMessage = new HttpRequestMessage();
+            CopyFromOriginalRequestContentAndHeaders(context, requestMessage);
+
+            requestMessage.RequestUri = targetUri;
+            requestMessage.Headers.Host = targetUri.Host;
+            requestMessage.Method = GetMethod(context.Request.Method);
+
+            return requestMessage;
+        }
+
+        private void CopyFromOriginalRequestContentAndHeaders(HttpContext context, HttpRequestMessage requestMessage)
+        {
+            var requestMethod = context.Request.Method;
+
+            if (!HttpMethods.IsGet(requestMethod) &&
+              !HttpMethods.IsHead(requestMethod) &&
+              !HttpMethods.IsDelete(requestMethod) &&
+              !HttpMethods.IsTrace(requestMethod))
+            {
+                var streamContent = new StreamContent(context.Request.Body);
+                requestMessage.Content = streamContent;
+            }
+
+            foreach (var header in context.Request.Headers)
+            {
+                requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+            }
+        }
+
+        private void CopyFromTargetResponseHeaders(HttpContext context, HttpResponseMessage responseMessage)
+        {
+            foreach (var header in responseMessage.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+
+            foreach (var header in responseMessage.Content.Headers)
+            {
+                context.Response.Headers[header.Key] = header.Value.ToArray();
+            }
+            context.Response.Headers.Remove("transfer-encoding");
+        }
+        private static HttpMethod GetMethod(string method)
+        {
+            if (HttpMethods.IsDelete(method)) return HttpMethod.Delete;
+            if (HttpMethods.IsGet(method)) return HttpMethod.Get;
+            if (HttpMethods.IsHead(method)) return HttpMethod.Head;
+            if (HttpMethods.IsOptions(method)) return HttpMethod.Options;
+            if (HttpMethods.IsPost(method)) return HttpMethod.Post;
+            if (HttpMethods.IsPut(method)) return HttpMethod.Put;
+            if (HttpMethods.IsTrace(method)) return HttpMethod.Trace;
+            return new HttpMethod(method);
+        }
+
+        private Uri BuildTargetUri(HttpRequest request)
+        {
+
+            if (request.Path.StartsWithSegments("/app/kibana", out var remainingPath))
+            {
+                return new Uri("http://localhost:5601" + remainingPath);
+            }
+
+            var referer = request.Headers["Referer"].FirstOrDefault();
+
+            if(referer != null)
+            {
+                var refererUri = new Uri(referer);
+                if (refererUri.Segments.Any() && refererUri.Segments[1] == "app/")
+                {
+                    return new Uri("http://localhost:5601" + request.Path.Value + request.QueryString);
+                }
+            }
+
+
+            return null;
+        }
+    }
+}
